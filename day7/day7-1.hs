126616ef49a3153b2d58355be334b3b84253fb24
Tobias Linton <tobias.linton@edgeware.tv>
2025-01-19 20:53:06 -0300

    Complete day 7, part 1

---
+++ day7/day7-1.hs
@@ -0,0 +1,37 @@
+import Data.List.Split (splitOn)
+
+data Equation = Equation { result :: Int, values :: [Int] }
+    deriving (Show)
+
+instance Read Equation where
+    readsPrec _ input =
+        [(Equation res vals, "")]
+        where
+            (res, vals) = getParts $ splitOn ":" input
+            getParts [] = (0, [])
+            getParts [r] = (read r, [])
+            getParts (r:vs:_) = (read r, map read $ words vs)
+
+combinations :: [a] -> Int -> [[a]]
+combinations _ 0 = [[]]
+combinations elements slots = [e:rest | e <- elements, rest <- combinations elements (slots - 1)]
+
+calculate :: [Int -> Int -> Int] -> [Int] -> Int
+calculate _ [] = 0
+calculate _ [v] = v
+calculate [] _ = 0
+calculate (f:fs) (v1:v2:vs) = calculate fs (f v1 v2 : vs)
+
+hasMatchingOperators :: Equation -> Bool
+hasMatchingOperators equation =
+    any matches functions
+    where
+        functions = combinations [(+), (*)] $ length (values equation) - 1
+        matches fns = calculate fns (values equation) == result equation
+
+main :: IO ()
+main = do
+    contents <- getContents
+    let equations = map read $ lines contents :: [Equation]
+    let matching = map snd $ filter fst $ map (\e -> (hasMatchingOperators e, result e)) equations
+    print $ sum matching
